676e13922e6e212934b96e9fbbbd14de588bc407
Kevin Philips Tanamas <220711789@students.uajy.ac.id>
2025-02-18 10:59:06 +0700

    add: bikin routes, dan layout

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+  },
+];
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Header routes={routes} />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+
+export default MainLayout;
