dada9250311face61665c70b8217d7c8afd232eb
NightOwlLab <ivantjandra18@gmail.com>
2025-02-27 01:51:02 +0700

    [FEAT] develop store page

---
+++ src/components/Item.jsx
@@ -0,0 +1,43 @@
+const Item = ({ picture, name, oldPrice, price }) => {
+    return (
+        <>
+            <a href="/detailproduct" className="detail-product-link col-md-2 text-start mx-1 my-2">
+                <div className="" >
+                    <div className="p-0" style={styles.card}>
+                    <img
+                        src={picture}
+                        alt={name}
+                        className="img-fluid"
+                        style={styles.image}
+                    />
+                    <p className="text-muted fs-5 text-start ms-2 mb-0">{name}</p>
+                    {oldPrice && (
+                        <>
+                            <span className="text-muted text-decoration-line-through fs-3 ms-2">
+                            ${oldPrice.toFixed(2)}
+                            </span>
+                            <i class="bi bi-arrow-right fs-3 ms-2"></i>
+                        </>
+                    )}
+                    <span className="fw-bold fs-3 ms-2">${price.toFixed(2)}</span>
+                    </div>
+            </div>
+            </a>
+        </>
+    );
+}
+
+const styles = {
+    image: {
+        aspectRatio: "1/1.5",
+        objectFit: "cover",
+        objectPosition: "center",
+        width: "100%",
+        margin: "0px 0px 10px 0px"
+    },
+    card: {
+        // border: "1px solid black",
+    }
+}
+
+export default Item;
