9f64f88aa3e2372ac72426af3f8bdc5ec66cec31
NightOwlLab <ivantjandra18@gmail.com>
2025-03-05 17:36:50 +0700

    [FEAT] add cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,7 +7,10 @@ import Image2 from '../assets/images/jeans.png';
 import Image3 from '../assets/images/tshirt.png';
 import Image4 from '../assets/images/pants.png';
 
+import Shop from '../assets/images/shop/shop.webp';
 
+// component item
+import Item from '../components/Item';
 
 const Cart = () => {
 
@@ -18,14 +21,25 @@ const Cart = () => {
         { id: 4, name: "Pants", price: 19.20, image: Image4, shopName: "Loop Wear", ammount: 3 },
     ]);
 
+    const items = [
+        { id: 1, name: "Phone Case", price: 6.2, image: Image1 },
+        { id: 2, name: "Hoodie Sweatshirt", price: 90.3, image: Image2, oldPrice: 129 },
+        { id: 3, name: "Basic T-Shirt", price: 27.54, image: Image3 },
+        { id: 4, name: "Baseball Cap", price: 19.2, image: Image4 },
+      ];
+
     return (
         <>
-            <section style={styles.section}>
+            <section className="py-3 py-sm-5" style={styles.section}>
 
 
                 <div className="container">
-                    <div className="row">
-                        <p className="fw-bold fs-2">Keranjang</p>
+
+                    <p className="fw-bold fs-2">Keranjang</p>
+
+                    <div className="row overflow-y-scroll" style={{
+                        height: '50vh',
+                    }}>
 
                         {
                             products.map((product) => 
@@ -47,12 +61,31 @@ const Cart = () => {
                                                     <h2 style={styles.cardTitle}>${product.price.toFixed(2)}</h2>
                                                 </div>
                                                 <div className="row">
-                                                    <div className="col ms-1">
-                                                        <div>
-                                                            <div className="btn" style={styles.ammountPart}><i class="bi bi-dash"></i></div>
-                                                            <div className="ammount" style={styles.ammountPart}>{product.ammount}</div>
-                                                            <div className="btn" style={styles.ammountPart}><i class="bi bi-plus-lg"></i></div>
+                                                    <div className="col-7 ms-1">
+                                                        <a href="/shop" className="text-decoration-none" style={{
+                                                            color: '#1A1816'
+                                                        }}>
+                                                            <div>
+                                                                <img src={Shop} alt="shop image" className="rounded-circle border border-black object-fit-cover" style={{
+                                                                    width: '30px',
+                                                                }}/>
+                                                                <p className="fw-bold fs-5 d-inline ms-2 ">Loopwear</p>
+                                                            </div>
+                                                        </a>
+                                                    </div>
+
+                                                    <div className="col">
+                                                        <div className="row column-gap-1">
+                                                            <div className="col d-flex justify-content-center align-items-center hoverable-item" style={styles.ammountPart}><i class="bi bi-dash"></i></div>
+                                                            <div className="col ammount d-flex justify-content-center align-items-center hoverable-item" style={styles.ammountPart}>{product.ammount}</div>
+                                                            <div className="col d-flex justify-content-center align-items-center hoverable-item" style={styles.ammountPart}><i class="bi bi-plus-lg"></i></div>
                                                         </div>
+                                                    </div>
+                                                </div>
+
+                                                <div className="row justify-content-end p-0">
+                                                    <div className="col text-end px-0 py-3">
+                                                        <button className="dark-btn px-5 py-1" >Buy</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -63,7 +96,7 @@ const Cart = () => {
                         }
                     </div>
 
-                    <div className="row">
+                    <div className="row mt-5">
                         <div className="col"><p className="fw-bold fs-2">Wishlist</p></div>
                         <div className="col">
                             <div class="row align-items-start mb-2">
@@ -78,6 +111,30 @@ const Cart = () => {
                                         <li><a class="dropdown-item" href="#"><i class="bi bi-plus-lg me-2"></i>Create New Category...</a></li>
                                     </ul>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col">
+                            <div className="row justify-content-between mb-4 overflow-y-scroll" style={{
+                        height: '50vh',
+                    }}>
+                            {products.map((product) => (
+              <Item key={product.id} picture={product.image} name={product.name} oldPrice={product.oldPrice} price={product.price} />
+            ))}
+            {products.map((product) => (
+              <Item key={product.id} picture={product.image} name={product.name} oldPrice={product.oldPrice} price={product.price} />
+            ))}
+            {products.map((product) => (
+              <Item key={product.id} picture={product.image} name={product.name} oldPrice={product.oldPrice} price={product.price} />
+            ))}
+            {products.map((product) => (
+              <Item key={product.id} picture={product.image} name={product.name} oldPrice={product.oldPrice} price={product.price} />
+            ))}
+            {products.map((product) => (
+              <Item key={product.id} picture={product.image} name={product.name} oldPrice={product.oldPrice} price={product.price} />
+            ))}
                             </div>
                         </div>
                     </div>
